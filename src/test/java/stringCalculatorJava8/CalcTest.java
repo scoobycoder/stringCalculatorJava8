821a4c9e85c6c587134e053a57e8f7cd59b7f638
scoobycoder <scoobycoder@gmail.com>
2016-03-20 17:23:29 -0400

    refactor tests to add a before for class setup

--- src/test/java/stringCalculatorJava8/CalcTest.java
+++ src/test/java/stringCalculatorJava8/CalcTest.java
@@ -2,19 +2,25 @@ package stringCalculatorJava8;
 
 import static org.junit.Assert.assertEquals;
 
+import org.junit.Before;
 import org.junit.Test;
 
 public class CalcTest {
 
+	private StringCalc stringCalc;
+
+	@Before
+	public void setup() {
+		stringCalc = new StringCalc();
+	}
+	
 	@Test
 	public void shouldAddTwoNumbers() {
-		StringCalc stringCalc = new StringCalc();
 		assertEquals(3, stringCalc.calc("1,2"));
 	}
 	
 	@Test
 	public void shouldAddThreeNumbers() {
-		StringCalc stringCalc = new StringCalc();
 		assertEquals(4, stringCalc.calc("0,1,3"));
 	}
 	
