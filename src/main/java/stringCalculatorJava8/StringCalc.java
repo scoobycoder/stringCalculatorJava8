7dffc22eb3b1edfdbc5acc642d34bb1a68577237
scoobycoder <scoobycoder@gmail.com>
2016-03-20 18:57:47 -0400

    switching to allow any delimiter, problems with arrays, shocking in java

--- src/main/java/stringCalculatorJava8/StringCalc.java
+++ src/main/java/stringCalculatorJava8/StringCalc.java
@@ -1,20 +1,31 @@
 package stringCalculatorJava8;
 
+import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.stream.Stream;
 
 public class StringCalc {
 
 	public int calc(String numbers) {
-		return sumStreamOfStringNumbers(createStreamOfStringNumbers(numbers));
+		Stream<String> numberStream = createStreamOfStringNumbers(numbers);
+		// numberStream.forEach(System.out::println);
+		return sumStreamOfStringNumbers(numberStream);
 	}
 
 	private Stream<String> createStreamOfStringNumbers(String numbers) {
-		return Arrays.stream(numbers.split(",|\n"));
+		String delimitter = String.valueOf(numbers.charAt(2));
+		ArrayList<String> numberArray = (ArrayList<String>) Arrays
+				.asList(numbers.split(delimitter));
+		numberArray.removeIf(item -> item == null || "".equals(item));
+
+		return Arrays.stream(
+				numberArray.toArray(new String[numberArray.size()])).map(
+				n -> n.replace("//", "").replace("\n", ""));
 	}
 
 	private int sumStreamOfStringNumbers(Stream<String> numberStream) {
-		return numberStream.mapToInt(n -> Integer.valueOf(n)).sum();
+		return numberStream.map(n -> n).mapToInt(n -> Integer.parseInt(n))
+				.sum();
 	}
 
 }
